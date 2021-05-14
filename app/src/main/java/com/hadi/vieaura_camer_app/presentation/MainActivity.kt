0e3745f3d2fd76f1b88f21be40aacecf96d96cee
hadimukaila <comradehadi@gmail.com>
2021-05-14 08:22:09 +0000

    backup pushed

--- app/src/main/java/com/hadi/vieaura_camer_app/presentation/MainActivity.kt
+++ app/src/main/java/com/hadi/vieaura_camer_app/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.hadi.vieaura_camer_app
+package com.hadi.vieaura_camer_app.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hadi.vieaura_camer_app.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
