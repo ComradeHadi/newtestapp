4825df5c0d2e67739d1e5f9e508cdb6edb3967ed
hadimukaila <comradehadi@gmail.com>
2021-05-14 11:27:35 +0000

    PicsTag changes

--- app/src/main/java/com/hadi/vieaura_camer_app/presentation/PicsTagsListAdapter.kt
+++ app/src/main/java/com/hadi/vieaura_camer_app/presentation/PicsTagsListAdapter.kt
@@ -5,49 +5,45 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.devtides.core.data.Note
-import com.devtides.memorynotes.R
-import kotlinx.android.synthetic.main.item_note.view.*
+import com.hadi.core.data.PicsTag
 import java.text.SimpleDateFormat
 import java.util.*
 
-class PicsTagsListAdapter(var notes: ArrayList<Note>, val actions: ListAction): RecyclerView.Adapter<PicsTagsListAdapter.NoteViewHolder>() {
+class PicsTagsListAdapter(var PicsTags: ArrayList<PicsTag>): RecyclerView.Adapter<PicsTagsListAdapter.PicsTagViewHolder>() {
 
-    fun updateNotes(newNotes: List<Note>) {
-        notes.clear()
-        notes.addAll(newNotes)
+    fun updatePicsTags(newPicsTags: List<PicsTag>) {
+        PicsTags.clear()
+        PicsTags.addAll(newPicsTags)
         notifyDataSetChanged()
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = NoteViewHolder(
-        LayoutInflater.from(parent.context).inflate(R.layout.item_note, parent, false)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = PicsTagViewHolder(
+        LayoutInflater.from(parent.context).inflate(R.layout.item_PicsTag, parent, false)
     )
 
-    override fun getItemCount() = notes.size
+    override fun getItemCount() = PicsTags.size
 
-    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
-        holder.bind(notes[position])
+    override fun onBindViewHolder(holder: PicsTagViewHolder, position: Int) {
+        holder.bind(PicsTags[position])
     }
 
-    inner class NoteViewHolder(view: View): RecyclerView.ViewHolder(view) {
+    inner class PicsTagViewHolder(view: View): RecyclerView.ViewHolder(view) {
 
-        private val layout = view.noteLayout
-        private val noteTitle = view.title
-        private val noteContent = view.content
-        private val noteDate = view.date
-        private val noteWords = view.wordCount
+        private val layout = view.PicsTagLayout
+        private val PicsTagTitle = view.title
+        private val PicsTagContent = view.content
+        private val PicsTagDate = view.date
+        private val PicsTagWords = view.wordCount
 
-        fun bind(note: Note) {
-            noteTitle.text = note.title
-            noteContent.text = note.content
+        fun bind(PicsTag: PicsTag) {
+            PicsTagTitle.text = PicsTag.title
+            PicsTagContent.text = PicsTag.content
 
             val sdf = SimpleDateFormat("MMM dd, HH:mm:ss")
-            val resultDate = Date(note.updateTime)
-            noteDate.text = "Last updated: ${sdf.format(resultDate)}"
+            val resultDate = Date(PicsTag.updateTime)
+            PicsTagDate.text = "Last updated: ${sdf.format(resultDate)}"
 
-            layout.setOnClickListener { actions.onClick(note.id) }
 
-            noteWords.text = "Words: ${note.wordCount}"
         }
     }
 }
